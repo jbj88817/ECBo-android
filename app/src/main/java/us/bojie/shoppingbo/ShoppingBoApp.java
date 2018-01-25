66efde3d8301d69a723c8fa5825c47ded0b7b454
Bojie Jiang <jiangbojie@gmail.com>
2018-01-25 00:13:19 -0800

    Initialized Icons

--- app/src/main/java/us/bojie/shoppingbo/ShoppingBoApp.java
+++ app/src/main/java/us/bojie/shoppingbo/ShoppingBoApp.java
@@ -2,6 +2,10 @@ package us.bojie.shoppingbo;
 
 import android.app.Application;
 
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+
+import us.bojie.latte.app.Latte;
+
 /**
  * Created by bojiejiang on 1/24/18.
  */
@@ -10,5 +14,9 @@ public class ShoppingBoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Latte.init(this)
+                .withIcon(new FontAwesomeModule())
+                .withApiHost("http://127.0.0.1/")
+                .configure();
     }
 }
