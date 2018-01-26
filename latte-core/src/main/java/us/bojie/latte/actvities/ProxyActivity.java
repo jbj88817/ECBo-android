37ea5e21a1ab37896e0a13f53248cc5ffeecc76c
Bojie Jiang <jiangbojie@gmail.com>
2018-01-26 00:19:11 -0800

    Using Fragmentation

---
+++ latte-core/src/main/java/us/bojie/latte/actvities/ProxyActivity.java
@@ -0,0 +1,10 @@
+package us.bojie.latte.actvities;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by bojiejiang on 1/26/18.
+ */
+
+public class ProxyActivity extends SupportActivity {
+}
