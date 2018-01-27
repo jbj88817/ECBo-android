99de1e2497581f7cf3ffa4a9c51350bdc358f738
Bojie Jiang <jiangbojie@gmail.com>
2018-01-26 23:43:13 -0800

    RestCreator creating RetrofitHolder, OKHttpHolder, RestServiceHolder

---
+++ latte-core/src/main/java/us/bojie/latte/net/HttpMethod.java
@@ -0,0 +1,15 @@
+package us.bojie.latte.net;
+
+/**
+ * Created by bojiejiang on 1/26/18.
+ */
+
+public enum HttpMethod {
+    GET,
+    POST,
+    POST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UPLOAD
+}
