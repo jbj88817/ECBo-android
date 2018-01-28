758cfc014ed3115d0777c03cfeeb543504971181
Bojie Jiang <jiangbojie@gmail.com>
2018-01-28 10:35:57 -0800

    LoaderCreator and style

---
+++ latte-core/src/main/java/us/bojie/latte/ui/LatteLoader.java
@@ -0,0 +1,15 @@
+package us.bojie.latte.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+
+/**
+ * Created by bojiejiang on 1/28/18.
+ */
+
+public class LatteLoader {
+    public static void showLoading(Context context, String type) {
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+
+    }
+}
---
+++ latte-core/src/main/java/us/bojie/latte/ui/LoaderStyle.java
@@ -0,0 +1,38 @@
+package us.bojie.latte.ui;
+
+/**
+ * Created by bojiejiang on 1/28/18.
+ */
+
+@SuppressWarnings("unused")
+public enum LoaderStyle {
+    BallPulseIndicator,
+    BallGridPulseIndicator,
+    BallClipRotateIndicator,
+    BallClipRotatePulseIndicator,
+    SquareSpinIndicator,
+    BallClipRotateMultipleIndicator,
+    BallPulseRiseIndicator,
+    BallRotateIndicator,
+    CubeTransitionIndicator,
+    BallZigZagIndicator,
+    BallZigZagDeflectIndicator,
+    BallTrianglePathIndicator,
+    BallScaleIndicator,
+    LineScaleIndicator,
+    LineScalePartyIndicator,
+    BallScaleMultipleIndicator,
+    BallPulseSyncIndicator,
+    BallBeatIndicator,
+    LineScalePulseOutIndicator,
+    LineScalePulseOutRapidIndicator,
+    BallScaleRippleIndicator,
+    BallScaleRippleMultipleIndicator,
+    BallSpinFadeLoaderIndicator,
+    LineSpinFadeLoaderIndicator,
+    TriangleSkewSpinIndicator,
+    PacmanIndicator,
+    BallGridBeatIndicator,
+    SemiCircleSpinIndicator,
+    CustomIndicator
+}
