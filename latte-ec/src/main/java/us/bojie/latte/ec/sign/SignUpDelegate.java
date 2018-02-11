e9d4f76b839f33bc4645121156294e01e024bd50
Bojie Jiang <jiangbojie@gmail.com>
2018-02-11 14:32:18 -0800

    SignUpDelegate

--- latte-ec/src/main/java/us/bojie/latte/ec/sign/SignUpDelegate.java
+++ latte-ec/src/main/java/us/bojie/latte/ec/sign/SignUpDelegate.java
@@ -2,16 +2,98 @@ package us.bojie.latte.ec.sign;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import us.bojie.latte.delegates.LatteDelegate;
 import us.bojie.latte.ec.R;
+import us.bojie.latte.ec.R2;
 
 /**
  * Created by bojiejiang on 2/11/18.
  */
 
 public class SignUpDelegate extends LatteDelegate {
+
+    @BindView(R2.id.edit_sign_up_name)
+    TextInputEditText mName;
+    @BindView(R2.id.edit_sign_up_email)
+    TextInputEditText mEmail;
+    @BindView(R2.id.edit_sign_up_phone)
+    TextInputEditText mPhone;
+    @BindView(R2.id.edit_sign_up_password)
+    TextInputEditText mPassword;
+    @BindView(R2.id.edit_sign_up_re_pwd)
+    TextInputEditText mRepeatPwd;
+
+    @OnClick(R2.id.btn_sign_up)
+    void onClickSignUp() {
+        if (checkForm()) {
+//            RestClient.builder()
+//                    .url("sign_up")
+//                    .params("","")
+//                    .success(new ISuccess() {
+//                        @Override
+//                        public void onSuccess(String response) {
+//
+//                        }
+//                    })
+//                    .build()
+//                    .post();
+            Toast.makeText(getContext(), "Signed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean checkForm() {
+        final String name = mName.getText().toString();
+        final String email = mEmail.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String password = mPassword.getText().toString();
+        final String rePassword = mRepeatPwd.getText().toString();
+        boolean isPass = true;
+
+        if (name.isEmpty()) {
+            mName.setError("Please input name!");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("Email address is wrong!");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        if (phone.isEmpty() || !Patterns.PHONE.matcher(phone).matches()) {
+            mPhone.setError("Phone number is wrong!");
+            isPass = false;
+        } else {
+            mPhone.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("Password at least 6 digits");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        if (rePassword.isEmpty() || rePassword.length() < 6 || !(rePassword.equals(password))) {
+            mRepeatPwd.setError("Re-enter correct password");
+            isPass = false;
+        } else {
+            mRepeatPwd.setError(null);
+        }
+
+        return isPass;
+    }
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_sign_up;
