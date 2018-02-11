d3432aa278e44f22e89ac1b812d161d78ea639a8
Bojie Jiang <jiangbojie@gmail.com>
2018-02-11 14:10:38 -0800

    Sign up layout

---
+++ latte-ec/src/main/java/us/bojie/latte/ec/sign/SignUpDelegate.java
@@ -0,0 +1,24 @@
+package us.bojie.latte.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import us.bojie.latte.delegates.LatteDelegate;
+import us.bojie.latte.ec.R;
+
+/**
+ * Created by bojiejiang on 2/11/18.
+ */
+
+public class SignUpDelegate extends LatteDelegate {
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_up;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}
