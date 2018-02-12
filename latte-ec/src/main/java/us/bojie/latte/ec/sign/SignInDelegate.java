8e018ee5a9c20ceb5932fe80f548640cacb4c291
Bojie Jiang <jiangbojie@gmail.com>
2018-02-11 18:09:46 -0800

    Sign in layout

---
+++ latte-ec/src/main/java/us/bojie/latte/ec/sign/SignInDelegate.java
@@ -0,0 +1,23 @@
+package us.bojie.latte.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import us.bojie.latte.delegates.BaseDelegate;
+
+/**
+ * Created by bojiejiang on 2/11/18.
+ */
+
+public class SignInDelegate extends BaseDelegate {
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}
