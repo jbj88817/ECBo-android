8ee00a2d85ddfff398a263d2d1c9588c292d7081
Bojie Jiang <jiangbojie@gmail.com>
2018-02-19 21:12:14 -0800

    Callback Translucent Activity

--- latte-core/src/main/java/us/bojie/latte/wechat/templates/WXEntryTemplate.java
+++ latte-core/src/main/java/us/bojie/latte/wechat/templates/WXEntryTemplate.java
@@ -1,15 +1,22 @@
 package us.bojie.latte.wechat.templates;
 
-import us.bojie.latte.actvities.ProxyActivity;
-import us.bojie.latte.delegates.LatteDelegate;
+import us.bojie.latte.wechat.BaseWXEntryActivity;
 
 /**
  * Created by bojiejiang on 2/18/18.
  */
 
-public class WXEntryTemplate extends ProxyActivity {
+public class WXEntryTemplate extends BaseWXEntryActivity {
+
     @Override
-    public LatteDelegate setRootDelegate() {
-        return null;
+    protected void onResume() {
+        super.onResume();
+        finish();
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    protected void onSignInSuccess(String userInfo) {
+
     }
 }
