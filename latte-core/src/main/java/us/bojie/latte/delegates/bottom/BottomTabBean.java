7dce1a7b7fb633dae091f3337e59497483a3bfa0
Bojie Jiang <jiangbojie@gmail.com>
2018-02-19 23:11:46 -0800

    BottomTabBean and ItemBuilder

---
+++ latte-core/src/main/java/us/bojie/latte/delegates/bottom/BottomTabBean.java
@@ -0,0 +1,23 @@
+package us.bojie.latte.delegates.bottom;
+
+/**
+ * Created by bojiejiang on 2/19/18.
+ */
+
+public final class BottomTabBean {
+    private final CharSequence ICON;
+    private final CharSequence TITLE;
+
+    public BottomTabBean(CharSequence icon, CharSequence title) {
+        this.ICON = icon;
+        this.TITLE = title;
+    }
+
+    public CharSequence getIcon() {
+        return ICON;
+    }
+
+    public CharSequence getTitle() {
+        return TITLE;
+    }
+}
