1d39e4c92d202f7bbff62558044d0f7cd1121f92
Bojie Jiang <jiangbojie@gmail.com>
2018-02-21 22:58:28 -0800

    delegate_index

--- latte-ec/src/main/java/us/bojie/latte/ec/main/index/IndexDelegate.java
+++ latte-ec/src/main/java/us/bojie/latte/ec/main/index/IndexDelegate.java
@@ -2,16 +2,37 @@ package us.bojie.latte.ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.joanzapata.iconify.widget.IconTextView;
+
+import butterknife.BindView;
 import us.bojie.latte.delegates.bottom.BottomItemDelegate;
 import us.bojie.latte.ec.R;
+import us.bojie.latte.ec.R2;
 
 /**
  * Created by bojiejiang on 2/21/18.
  */
 
 public class IndexDelegate extends BottomItemDelegate {
+    @BindView(R2.id.rv_index)
+    RecyclerView mRvIndex;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mSrlIndex;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconIndexScan;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mEtSearchView;
+    @BindView(R2.id.icon_index_message)
+    IconTextView mIconIndexMessage;
+    @BindView(R2.id.tb_index)
+    Toolbar mTbIndex;
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
