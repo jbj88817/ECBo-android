fd0f20a833c20e6e4ca241b5458722c80c035ab7
Bojie Jiang <jiangbojie@gmail.com>
2018-02-23 15:06:07 -0800

    Begin of DataConverter

--- latte-core/src/main/java/us/bojie/latte/ui/recycler/DataConverter.java
+++ latte-core/src/main/java/us/bojie/latte/ui/recycler/DataConverter.java
@@ -1,9 +1,21 @@
 package us.bojie.latte.ui.recycler;
 
+import java.util.ArrayList;
+
 /**
  * Created by bojiejiang on 2/21/18.
  */
 
 public abstract class DataConverter {
+
+    protected ArrayList<MultipleItemEntity> mEntities = new ArrayList<>();
+    private String mJsonData;
+
+    public abstract ArrayList<MultipleItemEntity> convert();
+
+    public DataConverter setJsonData(String jsonData) {
+        mJsonData = jsonData;
+        return this;
+    }
 
 }
