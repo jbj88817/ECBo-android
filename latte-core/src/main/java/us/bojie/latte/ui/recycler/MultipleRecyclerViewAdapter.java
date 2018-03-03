f728be1313db72074e6299a0f41dde9884e12223
Bojie Jiang <jiangbojie@gmail.com>
2018-03-03 10:12:38 -0800

    init MultipleRecyclerViewAdapter and related layouts

--- latte-core/src/main/java/us/bojie/latte/ui/recycler/MultipleRecyclerViewAdapter.java
+++ latte-core/src/main/java/us/bojie/latte/ui/recycler/MultipleRecyclerViewAdapter.java
@@ -1,11 +1,14 @@
 package us.bojie.latte.ui.recycler;
 
 import android.support.v7.widget.GridLayoutManager;
+import android.view.View;
 
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 
 import java.util.List;
+
+import us.bojie.latte.R;
 
 /**
  * Created by bojiejiang on 3/1/18.
@@ -18,16 +21,16 @@ public class MultipleRecyclerViewAdapter extends
 
     protected MultipleRecyclerViewAdapter(List<MultipleItemEntity> data) {
         super(data);
+        init();
     }
 
     @Override
-    protected void convert(MultipleViewHolder helper, MultipleItemEntity item) {
-
+    protected void convert(MultipleViewHolder holder, MultipleItemEntity entity) {
     }
 
     @Override
     public int getSpanSize(GridLayoutManager gridLayoutManager, int position) {
-        return 0;
+        return getData().get(position).getField(MultipleFields.SPAN_SIZE);
     }
 
     public static MultipleRecyclerViewAdapter create(List<MultipleItemEntity> data) {
@@ -36,5 +39,25 @@ public class MultipleRecyclerViewAdapter extends
 
     public static MultipleRecyclerViewAdapter create(DataConverter converter) {
         return new MultipleRecyclerViewAdapter(converter.convert());
+    }
+
+    private void init() {
+        // Save layout to array
+        addItemType(ItemType.TEXT, R.layout.item_multiple_text);
+        addItemType(ItemType.IMAGE, R.layout.item_multiple_image);
+        addItemType(ItemType.TEXT_IMAGE, R.layout.item_multiple_image_text);
+        addItemType(ItemType.BANNER, R.layout.item_multiple_banner);
+
+        // Listener
+        setSpanSizeLookup(this);
+        openLoadAnimation();
+
+        // Run anim multiple times
+        isFirstOnly(false);
+    }
+
+    @Override
+    protected MultipleViewHolder createBaseViewHolder(View view) {
+        return MultipleViewHolder.create(view);
     }
 }
