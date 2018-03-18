e315b2447a066a2f0d00ed25c6ff4fda19145898
Bojie Jiang <jiangbojie@gmail.com>
2018-03-17 23:01:22 -0700

    handleWebUrl

--- latte-core/src/main/java/us/bojie/latte/delegates/web/route/Router.java
+++ latte-core/src/main/java/us/bojie/latte/delegates/web/route/Router.java
@@ -1,6 +1,13 @@
 package us.bojie.latte.delegates.web.route;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
+
+import us.bojie.latte.delegates.LatteDelegate;
 import us.bojie.latte.delegates.web.WebDelegate;
+import us.bojie.latte.delegates.web.WebDelegateImpl;
 
 /**
  * Created by bojiejiang on 3/15/18.
@@ -20,6 +27,26 @@ public class Router {
     }
 
     public final boolean handleWebUrl(WebDelegate delegate, String url) {
+        if (url.contains("tel:")) {
+            callPhone(delegate.getContext(), url);
+            return true;
+        }
+
+        final LatteDelegate parentDelegate = delegate.getParentDelegate();
+        final WebDelegateImpl webDelegate = WebDelegateImpl.create(url);
+        if (parentDelegate == null) {
+            delegate.start(webDelegate);
+        } else {
+            parentDelegate.start(webDelegate);
+        }
+
         return true;
+    }
+
+    private void callPhone(Context context, String url) {
+        final Intent intent = new Intent(Intent.ACTION_DIAL);
+        final Uri data = Uri.parse(url);
+        intent.setData(data);
+        ContextCompat.startActivity(context, intent, null);
     }
 }
