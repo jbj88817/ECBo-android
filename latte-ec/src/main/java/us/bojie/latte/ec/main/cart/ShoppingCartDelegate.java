4605932fe2372296df7467a621a61571a10ee252
Bojie Jiang <jiangbojie@gmail.com>
2018-03-27 23:49:23 -0700

    Getting init shopping cart data

--- latte-ec/src/main/java/us/bojie/latte/ec/main/cart/ShoppingCartDelegate.java
+++ latte-ec/src/main/java/us/bojie/latte/ec/main/cart/ShoppingCartDelegate.java
@@ -3,15 +3,18 @@ package us.bojie.latte.ec.main.cart;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
 import us.bojie.latte.delegates.bottom.BottomItemDelegate;
 import us.bojie.latte.ec.R;
+import us.bojie.latte.net.RestClient;
+import us.bojie.latte.net.callback.ISuccess;
 
 /**
  * Created by bojiejiang on 3/22/18.
  */
 
-public class ShoppingCartDelegate extends BottomItemDelegate {
+public class ShoppingCartDelegate extends BottomItemDelegate implements ISuccess {
 
     @Override
     public Object setLayout() {
@@ -21,5 +24,21 @@ public class ShoppingCartDelegate extends BottomItemDelegate {
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
 
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        RestClient.builder()
+                .url("shop_cart.php")
+                .loader(getContext())
+                .success(this)
+                .build()
+                .get();
+    }
+
+    @Override
+    public void onSuccess(String response) {
+        Toast.makeText(getContext(), response, Toast.LENGTH_SHORT).show();
     }
 }
