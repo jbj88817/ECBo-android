74242a46dcf830c2275726c7df3d3975e3d5fca4
Bojie Jiang <jiangbojie@gmail.com>
2018-03-28 23:51:17 -0700

    Setup ShoppingCartDataConverter

---
+++ latte-ec/src/main/java/us/bojie/latte/ec/main/cart/ShoppingCartDataConverter.java
@@ -0,0 +1,14 @@
+package us.bojie.latte.ec.main.cart;
+
+import java.util.ArrayList;
+
+import us.bojie.latte.ui.recycler.DataConverter;
+import us.bojie.latte.ui.recycler.MultipleItemEntity;
+
+public class ShoppingCartDataConverter extends DataConverter {
+
+    @Override
+    public ArrayList<MultipleItemEntity> convert() {
+        return null;
+    }
+}
