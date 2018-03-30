e189243101541e99eba771924e16e2a01a3b5201
Bojie Jiang <jiangbojie@gmail.com>
2018-03-29 22:10:23 -0700

    Finished ShoppingCartDataConverter

---
+++ latte-ec/src/main/java/us/bojie/latte/ec/main/cart/ShopCartItemFields.java
@@ -0,0 +1,10 @@
+package us.bojie.latte.ec.main.cart;
+
+enum ShopCartItemFields {
+    TITLE,
+    DESC,
+    COUNT,
+    PRICE,
+    IS_SELECTED,
+    POSITION
+}
---
+++ latte-ec/src/main/java/us/bojie/latte/ec/main/cart/ShopCartItemType.java
@@ -0,0 +1,7 @@
+package us.bojie.latte.ec.main.cart;
+
+
+class ShopCartItemType {
+
+    static final int SHOP_CART_ITEM = 6;
+}
